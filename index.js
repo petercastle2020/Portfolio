bb2edc343a92936f916372a0ebf7c1fe97840a3a
Peter Castle <vagnerpetel2020@gmail.com>
2021-11-08 09:50:15 -0300

    add home/contact nav-bar and styles for it.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function slideBar() {
   setTimeout(function () {
     document.getElementById("nav-bar").classList.add("nav-bar");
-  }, 1500);
+  }, 1000);
+}
+
+function slideHomeBar() {
+  setTimeout(function () {
+    document.getElementById("home-nav-bar").classList.add("home-nav-bar");
+  }, 1000);
+}
+
+function slideContactBar() {
+  setTimeout(function () {
+    document.getElementById("contact-nav-bar").classList.add("contact-nav-bar");
+  }, 1000);
+}
+
+window.addEventListener("load", myInit, true);
+function myInit() {
+  slideBar();
+  slideHomeBar();
+  slideContactBar();
 }
